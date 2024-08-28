b82324b175890936f4dc768246351542d1989bbc
ParthSauguny <parthsauguny@gmail.com>
2024-08-29 01:31:33 +0530

    commit

--- src/components/Random.jsx
+++ src/components/Random.jsx
@@ -1,14 +1,27 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+const API_KEY = '32BHKkhfbMPRFy5FzUQ5KgPWHEuQmHCq';
 
 function Random() {
     const [gif , setGif] = useState('');
+    const API_URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}`;
+
+    async function fetchData(){
+      const {data} = await axios.get(API_URL);
+      const url = data.data.images.downsized_large.url;
+      setGif(url);
+    }
+
+    useEffect(() =>{
+      fetchData();
+    },[])
 
     function clickHandler(){
-
+      fetchData();
     }
 
   return (
-    <div className='flex flex-col bg-orange-600 border-4 border-green-500 items-center py-2 gap-y-1'>
+    <div className='flex flex-col bg-fuchsia-800 border-4 border-blue-500 items-center py-2 gap-y-1 my-4'>
         <h1>RANDOM GIFS</h1>
         <img src={gif} width="450"/>
 
--- src/components/Tagwale.jsx
+++ src/components/Tagwale.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useState , useEffect } from 'react';
+import axios from 'axios';
+const API_KEY = '32BHKkhfbMPRFy5FzUQ5KgPWHEuQmHCq';
 
 function Tagwale() {
+  const [tag , setTag] = useState('');
+
+  const changeHandler = (event) => {
+    setTag(event.target.value);
+  }
+
+  const [gif , setGif] = useState('');
+  const API_URL = `https://api.giphy.com/v1/gifs/random?api_key=${API_KEY}&tag=${tag}`;
+
+  async function fetchData(){
+    const {data} = await axios.get(API_URL);
+    const url = data.data.images.downsized_large.url;
+    setGif(url);
+  }
+
+  useEffect(() =>{
+    fetchData();
+  },[])
+
+  function clickHandler(){
+    fetchData();
+  }
+
   return (
-    <div>
-        
+    <div className='flex flex-col bg-orange-600 border-4 border-green-500 items-center py-2 gap-y-1 my-4'>
+        <h1>SEARCH FOR GIFS</h1>
+        <img src="" width="450"/>
+
+        <input type="text" className='bg-black bg-opacity-20' onChange={changeHandler}/>
+
+
+        <button onClick={clickHandler}
+        className='bg-white rounded-lg px-2 py-1'>
+            Generate
+        </button>
     </div>
   )
 }
